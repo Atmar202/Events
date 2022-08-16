d4b4209e400525b6328f8e24d4a80e1890ecb461
Atmar Peeduli <kruthal@kruthal.com>
2022-08-16 22:21:46 +0300

    Add validations and viewable forms depending on radio check

--- Events/wwwroot/js/site.js
+++ Events/wwwroot/js/site.js
@@ -9,3 +9,16 @@ switch (page[1].toLowerCase()) {
         document.getElementById("events").className = document.getElementById("events").className + " active";
     break;
 }
+
+function checkRadio() {
+
+    var radio = document.querySelector('#inlineRadio1');
+
+    if (radio.checked) {
+        document.getElementById("juriidiline").style.display = "none";
+        document.getElementById("eraisik").style.display = "initial";
+    } else {
+        document.getElementById("eraisik").style.display = "none";
+        document.getElementById("juriidiline").style.display = "initial";
+    }
+}
