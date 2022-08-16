cb52a2a447c2fe987f27eed12687e1e351965841
Atmar Peeduli <kruthal@kruthal.com>
2022-08-16 20:39:19 +0300

    Controller & Design for Participants page

---
+++ Events/Controllers/Participants.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Events.Controllers
+{
+    public class Participants : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            return View();
+        }
+    }
+}
